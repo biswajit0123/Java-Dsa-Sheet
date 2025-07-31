8f10981572ad097971a30090c812220f0e1daa79
biswajit0123 <biswajitmuduli0544@gmail.com>
2025-07-31 20:28:36 +0530

    threesum optimal approach

--- ARRAYS/Medium/threesum.java
+++ ARRAYS/Medium/threesum.java
@@ -52,17 +52,65 @@ public class threesum{
         return ans;
 
     }
+
+    //optimal
+    static List<List<Integer>> findTriplet3(int[] arr){
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+        int n = arr.length;
+       for(int i =0; i< n; i++){
+
+        if( i != 0 && arr[i] == arr[i-1] ){
+            continue;
+        } 
+       
+       int j = i+1;
+       int k = n-1;
+
+       while(j < k){
+        int sum = arr[i] + arr[j] + arr[k];
+        if(sum < 0){
+         j++;
+        }else if(sum > 0){
+            k--;
+        }else{
+            List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+            ans.add(temp);
+            j++;
+            k--;
+
+            while(j<k && arr[j] == arr[j-1]){
+                j++;
+            }
+            while(j<k && arr[k] == arr[k+1]){
+                k--;
+            }
+
+        }
+       }
+
+       }
+    return ans;
+
+    }
+
+
     public static void main(String[] ar){
        int[] arr = {-1,0,1,2,-1,-4};
        List<List<Integer>> l= new ArrayList<>();
        List<List<Integer>> l2= new ArrayList<>();
+       List<List<Integer>> l3= new ArrayList<>();
        //brute force 
        l = findTriplet(arr);
        System.out.println(l);
 
-       //brute force 
+       //optimal force 
        l2 = findTriplet2(arr);
        System.out.println(l2);
+
+       //optimal approach 
+       l3 = findTriplet3(arr);
+       System.out.println(l3);
 
     }
 }
