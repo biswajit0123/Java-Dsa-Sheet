da170f87816ed8ebd644d423241ac29c6e9cee32
biswajit0123 <biswajitmuduli0544@gmail.com>
2025-08-16 13:42:34 +0530

    tower of Hanoi hard

---
+++ Recuirsion/Tower_of_Hanoi.java
@@ -0,0 +1,20 @@
+
+
+public class Tower_of_Hanoi{
+
+   static void towerOfHanoi(int n , String src, String helper, String dest){
+       
+       if(n == 1){
+        System.out.println("move disk " + n + " from " + src + " to " + dest);
+        return;
+       }
+
+       towerOfHanoi(n-1, src, dest, helper);
+       System.out.println("move disk " + n + " from " + src + " to " + dest);
+       towerOfHanoi(n-1, helper, src, dest);
+   }
+
+    public static void main(String[] args){
+      towerOfHanoi(3, "S", "H", "D");
+    }
+}
