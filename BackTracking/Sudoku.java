6b17769e0cd58ca9736b2c6111505eee6afcd11b
Biswajit muduli <biswajitmuduli0544@gmail.com>
2025-10-22 20:01:28 +0530

    sudoku hard

---
+++ BackTracking/Sudoku.java
@@ -0,0 +1,95 @@
+public class Sudoku {
+
+    static boolean isSafe(int[][] sudoku, int row, int col, int digit) {
+
+        // horizonatal
+        for (int i = 0; i < 9; i++) {
+            if (sudoku[row][i] == digit) {
+                return false;
+            }
+        }
+
+        // vertical
+        for (int i = 0; i < 9; i++) {
+            if (sudoku[i][col] == digit) {
+                return false;
+            }
+        }
+        // grid
+
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (sudoku[i][j] == digit) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    static boolean sudokuSolver(int[][] sudoku, int row, int col) {
+        // base
+        if (row == 9) {
+            return true;
+        }
+
+        int nextrow = row, nextcol = col + 1;
+
+        if (col + 1 == 9) {
+            nextcol = 0;
+            nextrow = row + 1;
+        }
+        if (sudoku[row][col] != 0) {
+            return sudokuSolver(sudoku, nextrow, nextcol);
+        }
+        // kamm
+        for (int digit = 1; digit <= 9; digit++) {
+            if (isSafe(sudoku, row, col, digit)) {
+                sudoku[row][col] = digit;
+                if (sudokuSolver(sudoku, nextrow, nextcol)) {
+                    return true;
+                }
+                sudoku[row][col] = 0;
+
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] a) {
+        int[][] sudoku = {
+                { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+                { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+                { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+                { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+                { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+                { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+                { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+                { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+                { 0, 0, 0, 0, 8, 0, 0, 4, 9 }
+        };
+
+        if (sudokuSolver(sudoku, 0, 0)) {
+            System.out.println("solution exits");
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    System.out.print(sudoku[i][j] + " ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("solution not existss");
+              for (int i = 0; i < 9; i++) {
+                 for (int j = 0; j < 9; j++) {
+                    System.out.print(sudoku[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+}
